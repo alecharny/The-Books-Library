5a7258bccf4938d1410f87772bc2000e75f9d5e6
AdrienDevOps <62907151+AdrienDevOps@users.noreply.github.com>
2020-04-05 15:16:29 +0200

    Update book_library.py

--- book_library.py
+++ book_library.py
@@ -11,8 +11,14 @@ def scrap_book(book_to_add):
     url = f'https://www.googleapis.com/books/v1/volumes?q={book_to_add}'
     response = requests.get(url).json()
     resource = response['items'][0]['volumeInfo']
-    author = ', '.join(resource['authors'])
-    genre = ', '.join(resource['categories'])
+    try: 
+        author = ', '.join(resource['authors'])
+    except:
+        author = 'Not Found'
+    try: 
+        genre = ', '.join(resource['categories'])
+    except:
+        genre = 'Not Found'
     return author, genre
 
 def link_to_class_book(book_instance, name, author, genre):
@@ -21,12 +27,12 @@ def link_to_class_book(book_instance, name, author, genre):
     book_instance.genre = genre
 
 def add_to_html(name, author, genre):
-    with open('basic_html.html') as html_file_to_read:
+    with open('book_library.html') as html_file_to_read:
         soup = BeautifulSoup(html_file_to_read, 'html.parser')
         new_div = soup.new_tag('div')
         new_div.string = 'Book: %s\n / Author: %s\n / Genre: %s' % (name, author, genre)
         soup.body.insert(0, new_div)
-    with open('basic_html.html', 'w') as html_file_to_write:
+    with open('book_library.html', 'w') as html_file_to_write:
         html_file_to_write.write(str(soup))
 
 def nice_printing():
